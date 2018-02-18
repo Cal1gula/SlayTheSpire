a29c7dd976276cd64ff417826387ff3063ed239c
Jacob Horwath <jehorwath@gmail.com>
2018-02-17 21:13:05 -0500

    Added Runs table and InsertRun sproc

---
+++ SlayTheSpireDB/InsertRun.sql
@@ -0,0 +1,75 @@
+﻿CREATE PROCEDURE [dbo].[InsertRun]
+
+@JSON NVARCHAR(MAX) = N''
+
+AS
+
+BEGIN
+
+SET NOCOUNT ON;
+
+DECLARE @SQL NVARCHAR(MAX) = N'';
+--SET @JSON = N'D:\Steam\steamapps\common\SlayTheSpire\runs\IRONCLAD\1518908754.run';
+SET @SQL = N'SELECT * FROM OPENROWSET (BULK ''' + @JSON + ''', SINGLE_CLOB) AS j';
+
+DROP TABLE IF EXISTS #JSON;
+CREATE TABLE #JSON
+(RAWJSON NVARCHAR(MAX));
+
+INSERT INTO #JSON
+EXEC(@SQL);
+
+INSERT INTO Runs 
+	(
+	 [play_id]    
+	,[seed_played]
+    ,[build_version]
+    ,[character_chosen]
+    ,[playtime]
+    ,[victory]
+    ,[score]
+    ,[gold]
+    ,[floor_reached]
+    ,[campfire_rested]
+    ,[campfire_upgraded]
+    ,[purchased_purges]
+    ,[player_experience]
+    ,[killed_by]
+    ,[ascension_level]
+    ,[is_prod]
+    ,[is_daily]
+    ,[is_ascension_mode]
+    ,[is_trial]
+    ,[json_raw]
+	)
+SELECT 
+	 [play_id] = JSON_VALUE(RAWJSON, '$.play_id')
+    ,[seed_played] = JSON_VALUE(RAWJSON, '$.seed_played')
+    ,[build_version] = JSON_VALUE(RAWJSON, '$.build_version')
+    ,[character_chosen] = JSON_VALUE(RAWJSON, '$.character_chosen')
+    ,[playtime] = JSON_VALUE(RAWJSON, '$.playtime')
+    ,[victory] = JSON_VALUE(RAWJSON, '$.victory') 
+    ,[score] = JSON_VALUE(RAWJSON, '$.score')
+    ,[gold] = JSON_VALUE(RAWJSON, '$.gold')
+    ,[floor_reached] = JSON_VALUE(RAWJSON, '$.floor_reached')
+    ,[campfire_rested] = JSON_VALUE(RAWJSON, '$.campfire_rested')
+    ,[campfire_upgraded] = JSON_VALUE(RAWJSON, '$.campfire_upgraded')
+    ,[purchased_purges] = JSON_VALUE(RAWJSON, '$.purchased_purges')
+    ,[player_experience] = JSON_VALUE(RAWJSON, '$.player_experience')
+    ,[killed_by] = JSON_VALUE(RAWJSON, '$.killed_by')
+    ,[ascension_level] = JSON_VALUE(RAWJSON, '$.ascension_level')
+    ,[is_prod] = JSON_VALUE(RAWJSON, '$.is_prod')
+    ,[is_daily] = JSON_VALUE(RAWJSON, '$.is_daily')
+    ,[is_ascension_mode] = JSON_VALUE(RAWJSON, '$.is_ascension_mode')
+    ,[is_trial] = JSON_VALUE(RAWJSON, '$.is_trial')
+    ,[json_raw] = RAWJSON
+FROM 
+	#JSON j
+LEFT JOIN 
+	Runs r
+ON 
+	r.play_id = JSON_VALUE(j.RAWJSON, '$.play_id')
+WHERE 
+	r.play_id IS NULL;
+
+END
---
+++ SlayTheSpireDB/Runs.sql
@@ -0,0 +1,197 @@
+﻿CREATE TABLE [dbo].[Runs]
+(
+	[play_id] UNIQUEIDENTIFIER NOT NULL, 
+	[seed_played] BIGINT NOT NULL,
+    [build_version] nvarchar(255) NOT NULL,
+    [character_chosen] NVARCHAR(50) NOT NULL, 
+    [playtime] INT NOT NULL, 
+	[victory] BIT NULL DEFAULT 0,
+    [score] INT NULL DEFAULT 0, 
+    [gold] INT NULL DEFAULT 0, 
+    [floor_reached] INT NULL DEFAULT 0, 
+    [campfire_rested] INT NULL DEFAULT 0, 
+    [campfire_upgraded] INT NULL DEFAULT 0, 
+    [purchased_purges] INT NULL DEFAULT 0, 
+    [player_experience] INT NULL DEFAULT 0, 
+    [killed_by] NVARCHAR(255) NULL, 
+    [ascension_level] INT NULL DEFAULT 0, 
+    [is_prod] BIT NULL DEFAULT 0, 
+    [is_daily] BIT NULL DEFAULT 0, 
+    [is_ascension_mode] BIT NULL DEFAULT 0, 
+    [is_trial] BIT NULL DEFAULT 0, 
+    [json_raw] NVARCHAR(MAX) NOT NULL, 
+    PRIMARY KEY  ([play_id]) 
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Play time in seconds',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'playtime'
+GO
+
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Unique identifier of the Slay The Spire run',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'play_id'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Internal game seed number',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'seed_played'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Slay The Spire build number',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'build_version'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The character the player chose for the run',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'character_chosen'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Final score',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'score'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Final gold amount',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'gold'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Final floor reached by the player in the run',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'floor_reached'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Win or loss',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'victory'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Number of times the player upgraded cards at a campfire',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'campfire_upgraded'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Number of item purges the player purchased',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'purchased_purges'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Experience value for the player',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'player_experience'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Thing that killed the player and ended the run',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'killed_by'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Level of ascension',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'ascension_level'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Is this a prod run',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'is_prod'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Is the run for the daily quest',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'is_daily'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Is ascension mode on for this run',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'is_ascension_mode'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Is this a trial run',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'is_trial'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Raw JSON data',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Runs',
+    @level2type = N'COLUMN',
+    @level2name = N'json_raw'
