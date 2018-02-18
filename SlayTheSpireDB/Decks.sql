11d76b61e8f8c31fecc4e1698afa17c3c30e93a8
Jacob Horwath <jehorwath@gmail.com>
2018-02-18 18:51:43 -0500

    Added Decks table for deck listings

---
+++ SlayTheSpireDB/Decks.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[Decks]
+(
+	[play_id] UNIQUEIDENTIFIER NOT NULL , 
+    [card] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK_Decks] PRIMARY KEY ([play_id]), 
+    CONSTRAINT [FK_Decks_Runs] FOREIGN KEY ([play_id]) REFERENCES [Runs]([play_id]) ON DELETE CASCADE
+)
+
+GO
+
+CREATE INDEX [IX_Decks_card] ON [dbo].[Decks] ([card])
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The play_id from Runs table',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Decks',
+    @level2type = N'COLUMN',
+    @level2name = N'play_id'
