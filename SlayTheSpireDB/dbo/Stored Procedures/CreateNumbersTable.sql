e36dd8a87963ab16b8c69a25aaaf2a5ad537367d
Jacob Horwath <jehorwath@gmail.com>
2018-02-18 14:31:06 -0500

    Added Numbers table and CreateNumbersTable script

---
+++ SlayTheSpireDB/dbo/Stored Procedures/CreateNumbersTable.sql
@@ -0,0 +1,23 @@
+﻿
+CREATE PROCEDURE CreateNumbersTable 
+@UpperBound INT = 1000000
+AS
+BEGIN
+	SET NOCOUNT ON;
+DROP TABLE IF EXISTS [dbo].[Numbers];
+;WITH cteN(Number) AS
+(
+  SELECT ROW_NUMBER() OVER (ORDER BY s1.[object_id]) - 1
+  FROM sys.all_columns AS s1
+  CROSS JOIN sys.all_columns AS s2
+)
+SELECT [Number] INTO dbo.Numbers
+FROM cteN WHERE [Number] <= @UpperBound;
+
+CREATE UNIQUE CLUSTERED INDEX CIX_Number ON dbo.Numbers([Number])
+WITH 
+(
+  FILLFACTOR = 100      -- in the event server default has been changed
+--  ,DATA_COMPRESSION = ROW -- if Enterprise & table large enough to matter
+)
+END
