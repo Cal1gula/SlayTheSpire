6db836d6ac4775a7fd286f7885cc010bc58c0526
Jacob Horwath <jehorwath@gmail.com>
2018-02-18 16:41:40 -0500

    Added all per floor columns and additional constraints

--- SlayTheSpireDB/Floors.sql
+++ SlayTheSpireDB/Floors.sql
@@ -2,7 +2,11 @@
 (
 	[floornumber] INT NOT NULL  IDENTITY, 
     [play_id] UNIQUEIDENTIFIER NOT NULL, 
-    [path_taken] NVARCHAR(50) NOT NULL, 
+    [path_per_floor] NVARCHAR(50) NULL, 
+    [gold_per_floor] NVARCHAR(50) NULL, 
+    [max_hp_per_floor] INT NULL, 
+    [current_hp_per_floor] INT NULL, 
+    [vhp_percentage_per_floor] AS CONVERT(DECIMAL(18,2), (current_hp_per_floor * 1.0 / max_hp_per_floor * 1.0) * 100), 
     CONSTRAINT [PK_Floors] PRIMARY KEY ([floornumber], [play_id]), 
     CONSTRAINT [FK_Floors_Runs] FOREIGN KEY ([play_id]) REFERENCES [Runs]([play_id]) ON DELETE CASCADE
 )
@@ -18,13 +22,13 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level2name = N'floornumber'
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'The path choice the player took for this floor',
+    @value = N'The chosen path for this floor',
     @level0type = N'SCHEMA',
     @level0name = N'dbo',
     @level1type = N'TABLE',
     @level1name = N'Floors',
     @level2type = N'COLUMN',
-    @level2name = N'path_taken'
+    @level2name = 'path_per_floor'
 GO
 EXEC sp_addextendedproperty @name = N'MS_Description',
     @value = N'The unique id for this run references Runs table',
@@ -34,3 +38,21 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level1name = N'Floors',
     @level2type = N'COLUMN',
     @level2name = N'play_id'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The player gold at this floor',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Floors',
+    @level2type = N'COLUMN',
+    @level2name = N'gold_per_floor'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Maximum player hp at this floor',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Floors',
+    @level2type = N'COLUMN',
+    @level2name = N'max_hp_per_floor'
